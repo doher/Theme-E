308cc8e9543fb76b54b4239504031c66692422c9
doher <alexandr.pletezhov@gmail.com>
2019-01-31 23:33:59 +0300

    added solution to Task 4

--- Task4/dragAndDrop.js
+++ Task4/dragAndDrop.js
@@ -3,21 +3,108 @@ let table = document.createElement('table'),
     tr = document.createElement('tr'),
     td1 = document.createElement('td'),
     td2 = document.createElement('td'),
-    div1 = document.createElement('div'),
-    div2 = document.createElement('div'),
+    divStorage = document.createElement('div'),
+    divTempStorage = document.createElement('div'),
     input = document.createElement('input'),
-    button = document.createElement('button');
+    buttonAdd = document.createElement('button'),
+    buttonReset = document.createElement('button');
 
-div1.setAttribute('id', 'div1');
-div2.setAttribute('id', 'div2');
-button.innerHTML = 'Add Item';
+divStorage.setAttribute('id', 'storage');
+divTempStorage.setAttribute('id', 'tempStorage');
+input.setAttribute('type', 'text');
+input.setAttribute('id', 'inputItem');
+input.setAttribute('value', '');
+buttonAdd.setAttribute('class', 'addButton');
+buttonReset.setAttribute('class', 'resetItem');
+buttonAdd.innerHTML = 'Add Item';
+buttonReset.innerHTML = 'Reset';
 
 table.appendChild(tbody);
 tbody.appendChild(tr);
-td1.appendChild(div1);
-td2.appendChild(div2);
+td1.appendChild(divStorage);
+td2.appendChild(divTempStorage);
 tr.appendChild(td1);
 tr.appendChild(td2);
 document.body.appendChild(table);
 document.body.appendChild(input);
-document.body.appendChild(button);
+document.body.appendChild(buttonAdd);
+document.body.appendChild(buttonReset);
+
+let draggedItem = null;
+
+buttonAdd.addEventListener('click', function createItem() {
+    let div = document.createElement('div'),
+        span = document.createElement('span'),
+        tempStorage = document.getElementById('storage'),
+        item = document.getElementById('inputItem').value.trim(),
+        spanArray = document.getElementsByTagName('span');
+
+    if (item) {
+        div.setAttribute('class', 'item');
+        span.innerHTML = item;
+        div.appendChild(span);
+
+        if (spanArray.length) {
+            let isExist = false;
+
+            for (let i = 0; i < spanArray.length; ++i) {
+                if (spanArray[i].textContent === item) {
+                    isExist = true;
+                }
+            }
+
+            if (!isExist) {
+                tempStorage.appendChild(div);
+            }
+
+        } else {
+            tempStorage.appendChild(div);
+        }
+    }
+    document.getElementById('inputItem').value = '';
+}, false);
+
+buttonReset.addEventListener('click', function reset() {
+    let items = document.querySelectorAll('#tempStorage>.item'),
+        itemsLength = items.length;
+
+    if (itemsLength) {
+
+        for (let i = 0; i < itemsLength; ++i) {
+            divStorage.appendChild(items[i].cloneNode(true));
+            items[i].remove();
+        }
+    }
+}, false);
+
+divStorage.addEventListener('dragstart', function itemDragStart(e) {
+    e = e || window.event;
+
+    let item = e.target;
+
+    if (item.className !== 'item') {
+        return;
+    }
+
+    draggedItem = item;
+}, false);
+
+divStorage.addEventListener('dragend', function itemDragEnd(e) {
+    e = e || window.event;
+    draggedItem = null;
+}, false);
+
+divTempStorage.addEventListener('dragover', function divDragOver(e) {
+    e = e || window.event;
+    e.preventDefault();
+}, false);
+
+divTempStorage.addEventListener('drop', function divDrop(e) {
+    e = e || window.event;
+    e.preventDefault();
+
+    if (draggedItem) {
+        e.currentTarget.appendChild(draggedItem);
+    }
+
+}, false);
